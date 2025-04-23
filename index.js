26177aced950e7320f033ddc9baa63bd55f8c6d4
Muhamad Faisal Ilham <115516624+VsalCode@users.noreply.github.com>
2025-04-23 14:00:57 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import readline from "readline"
+import readline from "readline";
 import moment from "moment";
 
 const rl = readline.createInterface({
@@ -18,7 +18,7 @@ function menuUtama() {
   `);
   rl.question("masukkan angka: ", function (input) {
     if(moment(input, "DD-MM-YYYY").isValid()){
-      const result = moment(input, "DD-MM-YYYY").format("DD/MM/YYYY")
+      const result = moment(input, "DD-MM-YYYY").format("DD/MM/YYYY");
       console.log(result);
     }else{
       console.log("Format Tanggal Salah");
@@ -26,4 +26,30 @@ function menuUtama() {
   });
 }
 
-menuUtama()
+menuUtama();
+
+// TANPA PACKAGE
+
+function menuUtamaWithoutPackage() {
+  console.log(`
+    ========================================
+    |   # Program konversi ke DD/MM/YYY #  |
+    |--------------------------------------|
+    |        format harus DD-MM-YYY        |
+    ========================================
+  `);
+  rl.question("masukkan angka: ", function (input) {
+    const pisah = input.split("-");
+    // console.log(pisah);
+  
+    if(pisah.length <= 1){
+      console.log("Format Tanggal Anda Salah");
+    }else{
+      const result = pisah.join("/");
+      console.log(result);
+    }   
+  });
+}
+
+
+menuUtamaWithoutPackage();
