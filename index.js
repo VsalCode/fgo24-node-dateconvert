6f78d7901ccc092c14a661b1ee3ab70c35453be0
Muhamad Faisal Ilham <115516624+VsalCode@users.noreply.github.com>
2025-04-23 17:54:30 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import readline from "readline";
+import readline from "readline/promises";
 import moment from "moment";
 
 const rl = readline.createInterface({
@@ -6,9 +6,12 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-// MENU UTAMA
+/* --------------------------------------------- 
+DENGAN PACKAGE
+--------------------------------------------- */
 
-function menuUtama() {
+const firstMenu =  () => {
+  console.clear();
   console.log(`
     ========================================
     |   # Program konversi ke DD/MM/YYY #  |
@@ -16,40 +19,53 @@ function menuUtama() {
     |        format harus DD-MM-YYY        |
     ========================================
   `);
-  rl.question("masukkan angka: ", function (input) {
+  rl.question("masukkan angka: ", function(input) {
     if(moment(input, "DD-MM-YYYY").isValid()){
       const result = moment(input, "DD-MM-YYYY").format("DD/MM/YYYY");
       console.log(result);
-    }else{
-      console.log("Format Tanggal Salah");
-    }   
+    }else if(moment(input, "DD-MM-YYYY").isValid()){
+      const error = new Error("invalid_format");
+      throw error;
+    }
+    else{
+      const error = new Error("invalid_input");
+      throw error;
+    }
   });
+};
+
+try {
+  firstMenu();
+} catch (error) {
+  if(error.message === "invalid_input"){
+    console.log("Input tidak Valid !");
+  }
 }
 
-menuUtama();
 
-// TANPA PACKAGE
+/* --------------------------------------------- 
+TANPA PACKAGE
+--------------------------------------------- */
 
-function menuUtamaWithoutPackage() {
-  console.log(`
-    ========================================
-    |   # Program konversi ke DD/MM/YYY #  |
-    |--------------------------------------|
-    |        format harus DD-MM-YYY        |
-    ========================================
-  `);
-  rl.question("masukkan angka: ", function (input) {
-    const pisah = input.split("-");
-    // console.log(pisah);
+// function firstMenuWithoutPackage() {
+//   console.log(`
+//     ========================================
+//     |   # Program konversi ke DD/MM/YYY #  |
+//     |--------------------------------------|
+//     |        format harus DD-MM-YYY        |
+//     ========================================
+//   `);
+//   rl.question("masukkan angka: ", function (input) {
+//     const pisah = input.split("-");
+//     // console.log(pisah);
   
-    if(pisah.length <= 1){
-      console.log("Format Tanggal Anda Salah");
-    }else{
-      const result = pisah.join("/");
-      console.log(result);
-    }   
-  });
-}
+//     if(pisah.length <= 1){
+//       console.log("Format Tanggal Anda Salah");
+//     }else{
+//       const result = pisah.join("/");
+//       console.log(result);
+//     }   
+//   });
+// }
 
-
-menuUtamaWithoutPackage();
+// firstMenuWithoutPackage();
